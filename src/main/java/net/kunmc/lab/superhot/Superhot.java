94a55d38747f677871711d2473d0244ac58665ea
Maru <maru043@docomo.ne.jp>
2021-05-15 20:09:27 +0900

    initial commit

---
+++ src/main/java/net/kunmc/lab/superhot/Superhot.java
@@ -0,0 +1,15 @@
+package net.kunmc.lab.superhot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Superhot extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
