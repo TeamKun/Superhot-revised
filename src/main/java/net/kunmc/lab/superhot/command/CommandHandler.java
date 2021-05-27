d8b1b7e173b1549b8fca31c548d10681cb5b58e4
Maru <maru043@docomo.ne.jp>
2021-05-27 13:22:52 +0900

    giveコマンドの実装

--- src/main/java/net/kunmc/lab/superhot/command/CommandHandler.java
+++ src/main/java/net/kunmc/lab/superhot/command/CommandHandler.java
@@ -1,7 +1,9 @@
 package net.kunmc.lab.superhot.command;
 
 import net.kunmc.lab.superhot.Config;
+import net.kunmc.lab.superhot.Const;
 import net.kunmc.lab.superhot.GameManager;
+import net.kunmc.lab.superhot.helper.ItemHelper;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -9,6 +11,7 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -85,6 +88,45 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
                         sender.sendMessage(ChatColor.RED + "不明な項目です.");
                 }
                 break;
+            case "give":
+                if (args.length < 3) {
+                    sender.sendMessage(ChatColor.RED + "/superhot give <target> <item> [amount]");
+                    break;
+                }
+                List<Player> playerList = Bukkit.selectEntities(sender, args[1]).stream()
+                        .filter(x -> x instanceof Player)
+                        .map(x -> ((Player) x))
+                        .collect(Collectors.toList());
+                if (playerList.isEmpty()) {
+                    sender.sendMessage(ChatColor.RED + "プレイヤーが見つかりませんでした.");
+                    break;
+                }
+
+                String itemName = args[2];
+                ItemStack item = getSuperhotItem(itemName);
+                if (item == null) {
+                    sender.sendMessage(ChatColor.RED + "アイテムの名前が不正です.");
+                    break;
+                }
+
+                int amount = 1;
+                if (args.length > 3) {
+                    try {
+                        amount = Integer.parseInt(args[3]);
+                    } catch (NumberFormatException e) {
+                        sender.sendMessage(ChatColor.RED + "amountの値が不正です.");
+                        break;
+                    }
+                    if (amount < 1) {
+                        sender.sendMessage(ChatColor.RED + "amountの値は1以上の整数を指定してください.");
+                        break;
+                    }
+                }
+                item.setAmount(amount);
+
+                playerList.forEach(x -> x.getInventory().addItem(item));
+                sender.sendMessage(String.format(ChatColor.GREEN + "%d人のプレイヤーに%sを%d個配布しました.", playerList.size(), itemName, amount));
+                break;
             default:
                 sender.sendMessage(ChatColor.RED + "不明なコマンドです.");
         }
@@ -94,27 +136,63 @@ public class CommandHandler implements CommandExecutor, TabCompleter {
     @Override
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
         if (args.length == 1) {
-            return Stream.of("start", "stop", "config").filter(x -> x.startsWith(args[0])).collect(Collectors.toList());
+            return Stream.of("start", "stop", "config", "give").filter(x -> x.startsWith(args[0])).collect(Collectors.toList());
         }
 
         if (args.length == 2) {
             switch (args[0]) {
                 case "start":
-                    return Bukkit.getOnlinePlayers().stream().map(Player::getName).filter(x -> x.startsWith(args[1])).collect(Collectors.toList());
+                    return Bukkit.getOnlinePlayers().stream()
+                            .map(Player::getName)
+                            .filter(x -> x.startsWith(args[1]))
+                            .collect(Collectors.toList());
                 case "config":
-                    return Stream.of("AmmoAmount", "GlowMode").filter(x -> x.startsWith(args[1])).collect(Collectors.toList());
+                    return Stream.of("AmmoAmount", "GlowMode")
+                            .filter(x -> x.startsWith(args[1]))
+                            .collect(Collectors.toList());
+                case "give":
+                    return Stream.concat(Bukkit.getOnlinePlayers().stream().map(Player::getName), Stream.of("@a", "@r"))
+                            .filter(x -> x.startsWith(args[1]))
+                            .collect(Collectors.toList());
             }
         }
 
         if (args.length == 3) {
-            switch (args[1]) {
-                case "AmmoAmount":
-                    return Collections.singletonList("<amount>");
-                case "GlowMode":
-                    return Stream.of("true", "false").filter(x -> x.startsWith(args[2])).collect(Collectors.toList());
+            switch (args[0]) {
+                case "config":
+                    switch (args[1]) {
+                        case "AmmoAmount":
+                            return Collections.singletonList("<amount>");
+                        case "GlowMode":
+                            return Stream.of("true", "false").filter(x -> x.startsWith(args[2])).collect(Collectors.toList());
+                    }
+                    break;
+                case "give":
+                    return Stream.of("SuperhotGun", "SuperhotAmmo", "SuperhotSwapTool")
+                            .filter(x -> x.startsWith(args[2]))
+                            .collect(Collectors.toList());
+            }
+        }
+
+        if (args.length == 4) {
+            if (args[0].equals("give")) {
+                return Collections.singletonList("[amount]");
             }
         }
 
         return Collections.emptyList();
+    }
+
+    private ItemStack getSuperhotItem(String itemName) {
+        switch (itemName) {
+            case "SuperhotGun":
+                return ItemHelper.createItem(Const.gunMaterial, Const.gunName, 1);
+            case "SuperhotAmmo":
+                return ItemHelper.createItem(Const.ammoMaterial, Const.ammoName, 1);
+            case "SuperhotSwapTool":
+                return ItemHelper.createItem(Const.swapToolMaterial, Const.swapToolName, 1);
+            default:
+                return null;
+        }
     }
 }
